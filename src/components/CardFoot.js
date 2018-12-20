8feab1d670facaedd612eb85689b34190ccbcfa4
mingjun <>
2018-12-20 13:54:43 +0800

    Fix an issue where <textarea> doesn't shrink

--- src/components/CardFoot.js
+++ src/components/CardFoot.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
-
-
 class CardFoot extends React.Component {
     state = {
-        inputHeight: '34px'
+        rows: 1
     }
     handleSubmitComment = (event) => {
         if (event.key === 'Enter') {
@@ -12,17 +10,24 @@ class CardFoot extends React.Component {
         }
     }
     adjustInputHeight = (event) => {
-        const height = event.target.scrollHeight
-        this.setState(prevState => ({
-            inputHeight: `${height}px`
-        }))
+        event.target.style.height = '2.8rem' // 重置高度
+
+        const heightPerLine = 15 * 1.15 // 行高
+        const height = event.target.scrollHeight    // 高度
+        const rows = Math.floor(height / heightPerLine) // 行數
+
+        // 如果行數跟目前不一樣，再改變 state
+        if (this.state.rows != rows) {
+            this.setState(() => ({rows}))
+        }
+        event.target.style.height = 'auto'  // 配合 rows 調整高度
     }
     render(props) {
         return (
             <div className="user-comment">
                 <a className="user-comment__user-avatar" href="#">A</a>
                 <textarea
-                    style={{height: this.state.inputHeight}}
+                    rows={this.state.rows}
                     className="user-comment__input"
                     placeholder="回覆⋯⋯"
                     onKeyPress={this.handleSubmitComment}
