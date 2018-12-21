fc5be672f512153f76d7452a1c3586655f848e1e
mingjun <>
2018-12-21 17:27:27 +0800

    Split <CardComments> and create a subcomponent <SingleComment>

--- src/components/CardComments.js
+++ src/components/CardComments.js
@@ -1,55 +1,15 @@
 import React from 'react'
-import moment from 'moment'
-import 'moment/locale/zh-tw'
-moment.locale('zh-tw')
+import SingleComment from './SingleComment'
 
 const CardComments = (props) => (
     <div className="all-comments">
         {(props.comments.length > 0) &&  props.comments.map(comment => (
-            <div className="comment" key={comment.id}>
-                <a className="comment__author-avatar" href="#">
-                    {comment.authorName.slice(0, 1)}
-                </a>
-                <div>
-                    <p className="comment__body">
-                        <a
-                            className="comment__author-name"
-                            href="#"
-                        >
-                            {comment.authorName}
-                        </a>
-                        <span>
-                            {comment.text}
-                        </span>
-                    </p>
-                    <p className="comment-action">
-                        <span className="comment-action__like">
-                            讚
-                        </span>
-                        <span>．</span>
-                        <span className="comment-action__comment">
-                            回覆
-                        </span>
-                        {comment.authorID == props.currentUser.id && (
-                            <span>．</span>
-                        )}
-                        {comment.authorID == props.currentUser.id && (<span
-                            className="comment-action__delete"
-                            onClick={() => {props.deleteComment(comment.id)}}
-                        >
-                            刪除
-                        </span>
-                        )}
-                        <span>．</span>
-                        <span
-                            className="comment-action__timestamp"
-                            title={moment.unix(comment.publishedAt).format('LLL')}
-                        >
-                            {moment.unix(comment.publishedAt).fromNow()}
-                        </span>
-                    </p>
-                </div>
-            </div>
+            <SingleComment
+                key={comment.id}
+                comment={comment}
+                currentUser={props.currentUser}
+                deleteComment={props.deleteComment}
+            />
         ))}
     </div>
 )
---
+++ src/components/SingleComment.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import moment from 'moment'
+import 'moment/locale/zh-tw'
+moment.locale('zh-tw')
+
+const SingleComment = ({
+    comment, currentUser, deleteComment
+}) => (
+    <div className="comment">
+        <a className="comment__author-avatar" href="#">
+            {comment.authorName.slice(0, 1)}
+        </a>
+        <div>
+            <p className="comment__body">
+                <a
+                    className="comment__author-name"
+                    href="#"
+                >
+                    {comment.authorName}
+                </a>
+                <span>
+                    {comment.text}
+                </span>
+            </p>
+            <p className="comment-action">
+                <span className="comment-action__like">
+                    讚
+                </span>
+                <span>．</span>
+                <span className="comment-action__comment">
+                    回覆
+                </span>
+                {comment.authorID == currentUser.id && (
+                    <span>．</span>
+                )}
+                {comment.authorID == currentUser.id && (<span
+                    className="comment-action__delete"
+                    onClick={() => {deleteComment(comment.id)}}
+                >
+                    刪除
+                </span>
+                )}
+                <span>．</span>
+                <span
+                    className="comment-action__timestamp"
+                    title={moment.unix(comment.publishedAt).format('LLL')}
+                >
+                    {moment.unix(comment.publishedAt).fromNow()}
+                </span>
+            </p>
+        </div>
+    </div>
+)
+
+export default SingleComment
