d50b3caa2c36d3430c4e8f761df223a236c73958
mingjun <>
2018-12-21 13:14:10 +0800

    Use local state to handle input from <textarea>

--- src/components/CardFoot.js
+++ src/components/CardFoot.js
@@ -2,16 +2,22 @@ import React from 'react'
 
 class CardFoot extends React.Component {
     state = {
-        rows: 1
+        rows: 1,
+        inputValue: ''
+    }
+    handleInputChange = (event) => {
+        const value = event.target.value
+        this.setState(() => ({inputValue: value}))
     }
     handleSubmitComment = (event) => {
         // 按 Eneter 送出訊息，按 Shift + Enter 換行
         if (event.key === 'Enter' && !event.shiftKey) {
             event.preventDefault()  // 只按 Enter 時不換行
-            const textInput = event.target.value.trim()
+            const textInput = this.state.inputValue.trim()
+
             if (textInput) {
                 this.props.addComment(textInput)
-                event.target.value = ''
+                this.setState(() => ({inputValue: ''}))
                 // 重置 textarea 高度
                 if (this.state.rows > 1) {
                     this.setState(() => ({rows: 1}))
@@ -20,7 +26,11 @@ class CardFoot extends React.Component {
         }
     }
     adjustInputHeight = (event) => {
-        event.target.style.height = '2.8rem' // 重置高度
+        if (event.target.style.height !== '2.8rem') {
+            event.target.style.height = '2.8rem' // 重置高度
+            console.log(event.target.style.height)
+        }
+        
 
         const heightPerLine = 15 * 1.15 // 行高
         const height = event.target.scrollHeight    // 高度
@@ -31,6 +41,7 @@ class CardFoot extends React.Component {
             this.setState(() => ({rows}))
         }
         event.target.style.height = 'auto'  // 配合 rows 調整高度
+        console.log(event.target.style.height)
     }
     render(props) {
         return (
@@ -43,6 +54,8 @@ class CardFoot extends React.Component {
                     className="user-comment__input"
                     placeholder="回覆⋯⋯"
                     onKeyPress={this.handleSubmitComment}
+                    value={this.state.inputValue}
+                    onChange={this.handleInputChange}
                     onInput={this.adjustInputHeight}
                 >
                 </textarea>
