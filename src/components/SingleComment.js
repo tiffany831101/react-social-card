d5b1928a358cd67368a909c29aa73e6cf323b467
mingjun <>
2018-12-22 20:13:07 +0800

    Allow the user to hide comments

--- src/components/SingleComment.js
+++ src/components/SingleComment.js
@@ -3,90 +3,99 @@ import moment from 'moment'
 import 'moment/locale/zh-tw'
 moment.locale('zh-tw')
 
-const SingleComment = ({
-    comment, currentUser, deleteComment, handleLikeComment
-}) => (
-    <div className="comment">
-        <a className="comment__author-avatar" href="#">
-            {comment.authorName.slice(0, 1).toUpperCase()}
-        </a>
-        <div>
-            <div className="comment__body">
-            <p className="comment__text-section">
-                <a className="comment__author-name" href="#">
-                    {comment.authorName}
+class SingleComment extends React.Component {
+    state = {
+        style: {
+            display: 'flex'
+        }
+    }
+    hideElement = () => {
+        this.setState(prevState => ({style: {display: 'none'}}))
+    }
+    render() {
+        const {comment, currentUser, deleteComment, handleLikeComment} = this.props
+        return (
+            <div
+                className="comment"
+                style={this.state.style}
+            >
+                <a className="comment__author-avatar" href="#">
+                    {comment.authorName.slice(0, 1).toUpperCase()}
                 </a>
-                <span className="comment__text">{comment.text}</span>
-            </p>
-            {(comment.likes.length > 0) ? (
-                <span
-                    className="comment__like-count"
-                    title={
-                        comment.likes.map(obj => obj.userName).join('\n')
-                    }
-                >
-                    <i className="fas fa-thumbs-up" />
-                    <span>{comment.likes.length}</span>
-                </span>
-            ) : (
-                <span
-                    className="comment__like-count"
-                    title={
-                        comment.likes.map(obj => obj.userName).join('\n')
-                    }
-                    style={{visibility: 'hidden'}}
-                >
-                    <i className="fas fa-thumbs-up" />
-                    <span>{comment.likes.length}</span>
-                </span>
-            )}
+                <div>
+                    <div className="comment__body">
+                    <p className="comment__text-section">
+                        <a className="comment__author-name" href="#">
+                            {comment.authorName}
+                        </a>
+                        <span className="comment__text">{comment.text}</span>
+                    </p>
+                    {(comment.likes.length > 0) ? (
+                        <span
+                            className="comment__like-count"
+                            title={
+                                comment.likes.map(obj => obj.userName).join('\n')
+                            }
+                        >
+                            <i className="fas fa-thumbs-up" />
+                            <span>{comment.likes.length}</span>
+                        </span>
+                    ) : (
+                        <span
+                            className="comment__like-count"
+                            title={
+                                comment.likes.map(obj => obj.userName).join('\n')
+                            }
+                            style={{visibility: 'hidden'}}
+                        >
+                            <i className="fas fa-thumbs-up" />
+                            <span>{comment.likes.length}</span>
+                        </span>
+                    )}
+                    </div>
+                    <p className="comment-action">
+                        {comment.isLiked ? (
+                            <span
+                                className="comment-action__like comment-action__like--liked"
+                                onClick={() => {handleLikeComment(comment.id)}}
+                            >
+                                讚
+                            </span>
+                        ) : (
+                            <span
+                                className="comment-action__like"
+                                onClick={() => {handleLikeComment(comment.id)}}
+                            >
+                                讚
+                            </span>
+                        )}
+                        <span>．</span>
+                        <span
+                            className="comment-action__hide"
+                            onClick={this.hideElement}
+                        >
+                            隱藏
+                        </span>
+                        {comment.authorID === currentUser.id && <span>．</span>}
+                        {comment.authorID === currentUser.id && (<span
+                            className="comment-action__delete"
+                            onClick={() => {deleteComment(comment.id)}}
+                        >
+                            刪除
+                        </span>
+                        )}
+                        <span>．</span>
+                        <span
+                            className="comment-action__timestamp"
+                            title={moment.unix(comment.publishedAt).format('LLL')}
+                        >
+                            {moment.unix(comment.publishedAt).fromNow()}
+                        </span>
+                    </p>
+                </div>
             </div>
-            <p className="comment-action">
-                {comment.isLiked ? (
-                    <span
-                        className="comment-action__like comment-action__like--liked"
-                        onClick={() => {handleLikeComment(comment.id)}}
-                    >
-                        讚
-                    </span>
-                ) : (
-                    <span
-                        className="comment-action__like"
-                        onClick={() => {handleLikeComment(comment.id)}}
-                    >
-                        讚
-                    </span>
-                )}
-                {comment.authorID === currentUser.id && <span>．</span>}
-                {comment.authorID === currentUser.id && (<span
-                    className="comment-action__delete"
-                    onClick={() => {deleteComment(comment.id)}}
-                >
-                    刪除
-                </span>
-                )}
-                <span>．</span>
-                <span
-                    className="comment-action__timestamp"
-                    title={moment.unix(comment.publishedAt).format('LLL')}
-                >
-                    {moment.unix(comment.publishedAt).fromNow()}
-                </span>
-                {/* {(comment.likes.length > 0) && <span>．</span>}
-                {(comment.likes.length > 0) && (
-                    <span
-                        className="comment-action__like-count"
-                        title={
-                            comment.likes.map(obj => obj.userName).join('\n')
-                        }
-                    >
-                        <i className="fas fa-thumbs-up" />
-                        <span>{comment.likes.length}</span>
-                    </span>
-                )} */}
-            </p>
-        </div>
-    </div>
-)
+        )
+    }
+}
 
 export default SingleComment
