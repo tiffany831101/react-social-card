99deff5d10794e06bff1fa66c5b63d3b81ebb107
mingjun <>
2018-12-24 19:45:05 +0800

    Keep the last 5 comments unfolded at first

--- src/components/CardComments.js
+++ src/components/CardComments.js
@@ -1,20 +1,43 @@
 import React from 'react'
 import SingleComment from './SingleComment'
 
-const CardComments = ({
-    comments, currentUserName, currentUserID, handleDeleteComment, handleLikeComment
-}) => (
-    <div className="all-comments">
-        {(comments.length > 0) && comments.map(comment => (
-            <SingleComment
-                key={comment.id}
-                comment={comment}
-                currentUserID={currentUserID}
-                handleDeleteComment={handleDeleteComment}
-                handleLikeComment={handleLikeComment}
-            />
-        ))}
-    </div>
-)
+class CardComments extends React.Component {
+    state = {
+        isFolded: true
+    }
+    render() {
+        const {comments, currentUserName, currentUserID, handleDeleteComment, handleLikeComment} = this.props
+        return (
+            <div className="all-comments">
+                {comments.length > 5 && (
+                    <p
+                        className="all-comments__unfold"
+                        style={{
+                            display: this.state.isFolded || 'none',
+                            marginBottom: '0'
+                        }}
+                        onClick={() => {this.setState(() => ({isFolded: false}))}}
+                    >
+                        查看其他{comments.length - 5}則留言
+                    </p>
+                )}
+                {(comments.length > 0) && comments.map((comment, index) => {
+                    if (this.state.isFolded && comments.length - index > 5) {
+                        return null  // 舊的留言不渲染
+                    }
+                    return (
+                        <SingleComment
+                            key={comment.id}
+                            comment={comment}
+                            currentUserID={currentUserID}
+                            handleDeleteComment={handleDeleteComment}
+                            handleLikeComment={handleLikeComment}
+                        />
+                    )
+                })}
+            </div>
+        )
+    }
+}
 
 export default CardComments
