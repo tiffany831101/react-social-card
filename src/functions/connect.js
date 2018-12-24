521cd379582989eb66549f0eba63f26174a52ef0
mingjun <>
2018-12-24 16:13:43 +0800

    Add error handling to connect.js

--- src/functions/connect.js
+++ src/functions/connect.js
@@ -2,7 +2,11 @@ const connectTo = async (url) => {
     let response
     try {
         response = await fetch(url)
-        response = await response.json()
+        if (response.ok) {
+            response = await response.json()
+        } else {
+            throw new Error('抓不到資料')
+        }
     } catch(error) {console.log(error)}
     return response
 }
